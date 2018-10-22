344c005b1e4e3731f65ddbc69e985fa84a56f1ea
dfnino10 <df.nino10@uniandes.edu.co>
2018-10-22 15:01:52 -0500

    Se construyen de nuevo las bases de datos

--- edushareApp/admin.py
+++ edushareApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Estudiante, Materia, Carrera, Monitor, Horario
+
 
 # Register your models here.
+admin.site.register(Estudiante)
+admin.site.register(Materia)
+admin.site.register(Carrera)
+admin.site.register(Monitor)
+admin.site.register(Horario)
--- edushareApp/models.py
+++ edushareApp/models.py
@@ -4,60 +4,61 @@ from django.contrib.auth.models import User
 import datetime
 
 
-OPCIONES_CARRERA = {
-    'ADM' : 'Administración',
-    'ANTR' : 'Antropoloía',
-    'ARQ' : 'Arquitectura',
-    'ART' : 'Arte',
-    'BIO' : 'Biología',
-    'CPOL' : 'Ciencia Política',
-    'CONT' : 'Contaduría Internacional',
-    'DER' : 'Derecho',
-    'DIS' : 'Diseño',
-    'ECON' : 'Economía',
-    'FIL' : 'Filosofía',
-    'FISI' : 'Física',
-    'GEO' : 'Geociencias',
-    'GOB' : 'Gobierno y Asuntos Públicos',
-    'HIST' : 'Historia',
-    'HART' : 'Historia del Arte',
-    'IAM' : 'Ingeniería Ambiental',
-    'IBIO' : 'Ingeniería Biomédica',
-    'ICI' : 'Ingeniería Civil',
-    'ISIS' : 'Ingeniería de Sistemas y Computación',
-    'IETRI': 'Ingeniería Electrónica',
-    'IETRO': 'Ingeniería Electrónica',
-    'IIND': 'Ingeniería Industrial',
-    'IMEC': 'Ingeniería Mecánica',
-    'IQUI': 'Ingeniería Química',
-    'LENG': 'Lenguas y Cultura',
-    'LART': 'Licenciatura en Artes',
-    'LCN': 'Licenciatura en Ciencias Naturales',
-    'LCS': 'Licenciatura en Ciencias Sociales',
-    'LCEP': 'Licenciatura en Educación para la Primera Infancia',
-    'LH': 'Licenciatura en Humanidades',
-    'LM': 'Licenciatura en Matemáticas',
-    'LIT': 'Lteratura',
-    'MAT': 'Matemáticas',
-    'MED': 'Medicina',
-    'MIC': 'Microbiología',
-    'MUS': 'Música',
-    'ND' : 'Narrativas Digitales',
-    'PSI': 'Psicología',
-    'QUI': 'Química',
-}
+OPCIONES_CARRERA = (
+    ("ADM" , 'Administración'),
+    ("ANTR" , 'Antropoloía'),
+    ("ARQ" , 'Arquitectura'),
+    ("ART" , 'Arte'),
+    ("BIO" , 'Biología'),
+    ("CPOL" , 'Ciencia Política'),
+    ("CONT" , 'Contaduría Internacional'),
+    ("DER" , 'Derecho'),
+    ("DIS" , 'Diseño'),
+    ("ECON" , 'Economía'),
+    ("FIL" , 'Filosofía'),
+    ("FISI" , 'Física'),
+    ("GEO" , 'Geociencias'),
+    ("GOB" , 'Gobierno y Asuntos Públicos'),
+    ("HIST" , 'Historia'),
+    ("HART" , 'Historia del Arte'),
+    ("IAM" , 'Ingeniería Ambiental'),
+    ("IBIO" , 'Ingeniería Biomédica'),
+    ("ICI" , 'Ingeniería Civil'),
+    ("ISIS" , 'Ingeniería de Sistemas y Computación'),
+    ("IETRI", 'Ingeniería Electrónica'),
+    ("IETRO", 'Ingeniería Electrónica'),
+    ("IIND", 'Ingeniería Industrial'),
+    ("IMEC", 'Ingeniería Mecánica'),
+    ("IQUI", 'Ingeniería Química'),
+    ("LENG", 'Lenguas y Cultura'),
+    ("LART", 'Licenciatura en Artes'),
+    ("LCN", 'Licenciatura en Ciencias Naturales'),
+    ("LCS", 'Licenciatura en Ciencias Sociales'),
+    ("LCEP", 'Licenciatura en Educación para la Primera Infancia'),
+    ("LH", 'Licenciatura en Humanidades'),
+    ("LM", 'Licenciatura en Matemáticas'),
+    ("LIT", 'Lteratura'),
+    ("MAT", 'Matemáticas'),
+    ("MED", 'Medicina'),
+    ("MIC", 'Microbiología'),
+    ("MUS", 'Música'),
+    ("ND" , 'Narrativas Digitales'),
+    ("PSI", 'Psicología'),
+    ("QUI", 'Química'),
+)
 # Create your models here.
 class Materia(models.Model):
-    nombre= nombre = models.CharField(max_length=10, default='')
+    nombre= models.CharField(max_length=10, default='')
+    carrera= models.ForeignKey('Carrera')
 
 class Carrera(models.Model):
     nombre = models.CharField(max_length=10, choices=OPCIONES_CARRERA, default='')
     materias = models.ManyToManyField(Materia)
 
 class Estudiante(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE())
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=200, default ='')
-    carrera = models.ForeignKey(Carrera)
+    carrera = models.ForeignKey(Carrera,on_delete=models.CASCADE)
     email = models.EmailField(max_length=200)
     imagen = models.ImageField(upload_to='imagen_perfil', blank= True)
 
@@ -65,26 +66,21 @@ class Estudiante(models.Model):
 class Horario(models.Model):
     fecha = models.DateField()
     hora_inicio = models.TimeField()
-    hora_inicio = models.TimeField()
+    hora_fin = models.TimeField()
 
 class Monitor(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE())
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=200, default='')
-    carrera = models.CharField(max_length=10, choices=OPCIONES_CARRERA, default='')
+    carrera = models.ForeignKey(Carrera,on_delete=models.CASCADE)
     email = models.EmailField(max_length=200)
     imagen = models.ImageField(upload_to='imagen_perfil', blank=True)
     disponibilidad = models.ManyToManyField(Horario)
 
 class monitoria (models.Model):
-    estiduante = models.ForeignKey(Estudiante)
-    monitor = models.ForeignKey(Monitor)
+    estiduante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    monitor = models.ForeignKey(Monitor, on_delete=models.CASCADE)
     lugar = models.CharField(max_length=200, default='')
     materia = models.CharField(max_length=200, default='')
     materia = models.CharField(max_length=200, default='')
     tema = models.CharField(max_length=200, default='')
-
-
-
-
-
 
