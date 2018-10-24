e4a30c75b70f578a2c9b1fbd8691a64b10a503b0
dfnino10 <df.nino10@uniandes.edu.co>
2018-10-23 22:35:58 -0500

    models revisados

--- edushareApp/models.py
+++ edushareApp/models.py
@@ -3,7 +3,6 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 import datetime
 
-
 OPCIONES_CARRERA = (
     ("ADM" , 'Administración'),
     ("ANTR" , 'Antropoloía'),
@@ -47,12 +46,23 @@ OPCIONES_CARRERA = (
     ("QUI", 'Química'),
 )
 # Create your models here.
-class Materia(models.Model):
-    nombre= models.CharField(max_length=10, default='')
 
 class Carrera(models.Model):
-    nombre = models.CharField(max_length=10, choices=OPCIONES_CARRERA, default='')
-    materias = models.ManyToManyField(Materia)
+    nombre = models.CharField(max_length=100, choices=OPCIONES_CARRERA, default='')
+
+class Materia(models.Model):
+    nombre= models.CharField(max_length=50, default='')
+    carrera= models.ForeignKey(Carrera, on_delete=models.CASCADE, default='')
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length = 50, default = '')
+    descuento = models.FloatField(default = 0.0)
+
+class Nivel(models.Model):
+    nombre = models.CharField(max_length = 50, default = '')
+    limite = models.IntegerField(default = 0)
+    fee = models.FloatField(default = 0.0)
+    cobromaximo = models.FloatField(default = 0.0)
 
 class Estudiante(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -60,12 +70,13 @@ class Estudiante(models.Model):
     carrera = models.ForeignKey(Carrera,on_delete=models.CASCADE)
     email = models.EmailField(max_length=200)
     imagen = models.ImageField(upload_to='imagen_perfil', blank= True)
-
+    categoria = models.ForeignKey(Categoria, on_delete = models.SET_NULL, null = True)
 
 class Horario(models.Model):
     fecha = models.DateField()
     hora_inicio = models.TimeField()
     hora_fin = models.TimeField()
+    disponibilidad = models.BooleanField(default = False)
 
 class Monitor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -73,13 +84,13 @@ class Monitor(models.Model):
     carrera = models.ForeignKey(Carrera,on_delete=models.CASCADE)
     email = models.EmailField(max_length=200)
     imagen = models.ImageField(upload_to='imagen_perfil', blank=True)
-    disponibilidad = models.ManyToManyField(Horario)
+    horario = models.ManyToManyField(Horario)
+    nivel = models.ForeignKey(Nivel, on_delete = models.SET_NULL, null = True)
 
-class monitoria (models.Model):
-    estiduante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+class Monitoria (models.Model):
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
     monitor = models.ForeignKey(Monitor, on_delete=models.CASCADE)
+    horario = models.ForeignKey(Horario, on_delete = models.CASCADE, default='')
     lugar = models.CharField(max_length=200, default='')
     materia = models.CharField(max_length=200, default='')
-    materia = models.CharField(max_length=200, default='')
     tema = models.CharField(max_length=200, default='')
-
