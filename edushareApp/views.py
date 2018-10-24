24156800924c9f9d37ee8167c9cab7a9e64990fe
JuanFelipeSanchez <jf.sanchezg@uniandes.edu.co>
2018-10-24 13:59:52 -0500

    templates y coneccion con index

--- edushareApp/views.py
+++ edushareApp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from .models import  Estudiante
+from .models import Estudiante, Materia, Carrera, Monitor, Horario
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html', {})
+    materias = Materia.objects.order_by('nombre')
+    monitores = Monitor.objects.order_by('nombre')
+    info_destacados = {"materia":materias, "monitor":monitores}
+    return render(request, 'edushareApp/Index.html', info_destacados)
